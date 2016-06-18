d09b215a980c44b794ddf4537d080e06003a9f2c
eignatik <light.ji@yandex.ru>
2016-06-18 18:53:50 +0300

    broker: add getTopics method, mda: add create tables (clients, subscribe) methods, add checking (is created this tables), add getTopicList method; entities: add client and subscribe classes

---
+++ MessageBroker/src/main/java/com/deveignatik/MessageBroker/LibKernel/entities/Client.java
@@ -0,0 +1,40 @@
+package com.deveignatik.MessageBroker.LibKernel.entities;
+
+/**
+ * 6:40 PM.
+ */
+public class Client {
+    private long id;
+    private String name;
+    private int lastmessageid;
+
+    public Client(long id, String name, int lastmessageid) {
+        this.id = id;
+        this.name = name;
+        this.lastmessageid = lastmessageid;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getLastmessageid() {
+        return lastmessageid;
+    }
+
+    public void setLastmessageid(int lastmessageid) {
+        this.lastmessageid = lastmessageid;
+    }
+}
---
+++ MessageBroker/src/main/java/com/deveignatik/MessageBroker/LibKernel/entities/Subscribe.java
@@ -0,0 +1,7 @@
+package com.deveignatik.MessageBroker.LibKernel.entities;
+
+/**
+ * 6:40 PM.
+ */
+public class Subscribe {
+}
